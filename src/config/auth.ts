8934ed1c0b608ff9fb880cb2ed3c86fc60dfdd3c
Idelmario Macedo <idelmariocorreia@hotmail.com>
2022-06-01 12:26:39 -0300

    add refresh_token requirements, password recovery requirements, and test refactoring

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'bf60ea46c2632ef8f5a6235807ac381b',
+  expires_in_token: '15m',
+  secret_refresh_token: '125c41faf66e2c351bff02def3b22cab',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
---
+++ src/modules/accounts/useCases/resetPassword/ResetPasswordUserController.ts
@@ -0,0 +1,20 @@
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+
+import { ResetPasswordUserUserCase } from './ResetPasswordUserUseCase';
+
+class ResetPasswordUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { token } = request.query;
+    const { password } = request.body;
+    const resetPasswordUserUserCase = container.resolve(
+      ResetPasswordUserUserCase,
+    );
+
+    await resetPasswordUserUserCase.execute({ token: String(token), password });
+
+    return response.send();
+  }
+}
+
+export { ResetPasswordUserController };
